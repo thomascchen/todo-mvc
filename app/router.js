fae8edbd8630d1fea34aef173f70b73afd47e232
Thomas Chen <chen.thomas.c@gmail.com>
2015-07-30 15:59:05 -0400

    Finish tutorial

--- app/router.js
+++ app/router.js
@@ -2,14 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function() {
-  this.resource('todos', { path: '/' });
-  this.route('todos', function() {
-    this.route('active');
-    this.route('complete');
+  this.resource('todos', { path: '/' }, function() {
+      this.route('active');
+      this.route('complete');
   });
 });
 
--- app/routes/todos.js
+++ app/routes/todos.js
@@ -5,15 +5,12 @@ export default Ember.Route.extend({
         return this.store.find('todo');
     },
     actions: {
-        createTodo: function(newTitle) {
+        createTodo: function (newTitle) {
             // Create the new Todo model
             var todo = this.store.createRecord('todo', {
                 title: newTitle,
                 isCompleted: false
             });
-
-            // Clear the "New Todo" text field
-            this.controllerFor('todos').set('newTitle', '');
 
             // Save the new model
             todo.save();
